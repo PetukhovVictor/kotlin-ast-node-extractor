1e3fa63612c11228f98ca312bfd732b0498bdc85
Victor Petukhov <i@victor.am>
2017-11-25 13:11:47 +0300

    Implemented writing subast per file

--- main.py
+++ main.py
@@ -19,19 +19,33 @@ if len(sys.argv) <= 3:
     sys.stderr.write('AST extraction nodes folder (output) not specified.\n')
     exit()
 
+subtree_per_file = len(sys.argv) == 5 and sys.argv[4] == '--subtree-per-file'
+
 target = sys.argv[1]
 folder_input = sys.argv[2]
 folder_output = sys.argv[3]
+
 
 def ast_file_process(filename):
     root = AstReader.read(filename)
     ast_node_extractor = AstNodeExtractor(root, target)
     extraction_nodes = ast_node_extractor.extract()
 
-    relative_filename = os.path.relpath(filename, folder_input)
-    AstWriter.write(folder_output, relative_filename, extraction_nodes)
+    def ast_write(filename, extraction_nodes):
+        relative_filename = os.path.relpath(filename, folder_input)
+        AstWriter.write(folder_output, relative_filename, extraction_nodes)
+        TimeLogger.console_output(None, prefix='Processing ' + relative_filename + '...')
 
-    TimeLogger.console_output(None, prefix='Processing ' + relative_filename + '...')
+    if subtree_per_file:
+        ast_node_counter = 1
+        for extraction_node in extraction_nodes:
+            basename = os.path.splitext(filename)[0]
+            extension = os.path.splitext(filename)[1]
+            new_filename = basename + '.' + str(ast_node_counter) + extension
+            ast_write(new_filename, [extraction_node])
+            ast_node_counter += 1
+    else:
+        ast_write(filename, extraction_nodes)
 
 
 extraction_time = FilesWalker.walk(folder_input, ast_file_process)
